dfb4afe67dc591d6325915b9345da284c34d7316
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-05 18:45:12 -0500

    scaffolded /blog route

---
+++ pages/blog/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { getAllPosts } from '../../utils/mdx'
+
+export default function Blog({ posts }) {
+  return (
+    <div>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <Link href={`posts/${post.slug}`}>
+              <a>{post.frontmatter.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const posts = getAllPosts()
+
+  return {
+    props: { posts },
+  }
+}
