d6772a862854f052d0336a7407a661ab9d4d5ae4
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-05 18:35:25 -0500

    implemented getAllPosts function in utils/mdx.js

---
+++ utils/mdx.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { bundleMDX } from 'mdx-bundler'
+
+export const POSTS_PATH = path.join(process.cwd(), 'data/posts')
+
+export const getSourceOfFile = (fileName) => {
+  return fs.readFileSync(path.join(POSTS_PATH, fileName))
+}
+
+export const getAllPosts = () => {
+  return fs
+    .readdirSync(POSTS_PATH)
+    .filter((path) => /\.mdx?$/.test(path))
+    .map((fileName) => {
+      const source = getSourceOfFile(fileName)
+      const slug = fileName.replace(/\.mdx?$/, '')
+      const { data } = matter(source)
+
+      return {
+        frontmatter: data,
+        slug: slug,
+      }
+    })
+}
