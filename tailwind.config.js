4d01d2c57de0277418e78a97f7ba15598df36142
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-09 18:46:25 -0500

    remove custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: require('tailwindcss-open-color'),
+    extend: {
+      fontFamily: {
+        system: [...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 }
