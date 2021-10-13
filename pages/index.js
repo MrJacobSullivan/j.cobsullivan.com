54b1e6e4d3338193b964ec4a072184393046a881
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-12 23:49:08 -0500

    added tailwind typography

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-import tw, { styled } from 'twin.macro'
+import tw from 'twin.macro'
 
 const Main = tw.div`text-green-5`
-
-const BlueMain = styled(Main)(() => ({
-  '> p': tw`text-xl`,
-}))
-
-const RedMain = styled(Main)(({ open }) => [
-  tw`bg-red-2 text-red-8`,
-  open && tw`border border-red-9`,
-])
 
 export default function Home() {
   return (
