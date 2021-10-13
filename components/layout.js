2e6d6d8c3d1364bb87f8c1b73b2fa09629d4baf6
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-12 23:40:49 -0500

    fixed build bug

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,20 @@
 import Header from './Header'
 import Footer from './Footer'
 
+import tw, { styled } from 'twin.macro'
+
+const Screen = tw.div`flex flex-col items-center justify-center w-screen`
+
+const Content = tw.div`flex flex-col items-center w-[65%] min-h-screen`
+
 export default function Layout({ children }) {
   return (
-    <Container>
-      <Header />
-      <main className='flex-grow w-full py-5'>{children}</main>
-      <Footer />
-    </Container>
-  )
-}
-
-export const Container = ({ children }) => {
-  return (
-    <div className='flex flex-col items-center justify-center w-screen'>
-      <div className='flex flex-col items-center w-[65%] min-h-screen'>{children}</div>
-    </div>
+    <Screen>
+      <Content>
+        <Header />
+        <main tw='flex-grow w-full py-5'>{children}</main>
+        <Footer />
+      </Content>
+    </Screen>
   )
 }
