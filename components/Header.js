a8d78e2314e936f4f0110d9ebd417d07435d0e5b
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-26 23:07:49 -0500

    added styles to Header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,41 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import tw from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 import ThemeButton from './ThemeButton'
 import { routes } from '../config'
+
+const StyledHeader = styled.header`
+  ${tw`sticky top-0 z-50 flex w-full h-10 px-5 py-10 mt-2 rounded dark:bg-gray-8 bg-gray-2`};
+
+  div.title {
+    ${tw`flex items-center w-1/2`};
+
+    a {
+      ${tw`text-xl font-light transition duration-150`};
+
+      &:hover {
+        ${tw`dark:text-violet-3 text-green-5`};
+      }
+    }
+  }
+
+  div.nav {
+    ${tw`flex items-center justify-end w-1/2`};
+
+    a {
+      ${tw`font-light transition duration-150`};
+
+      &:hover {
+        ${tw`dark:text-violet-3 text-green-5`};
+      }
+    }
+
+    a,
+    button {
+      ${tw`ml-6`};
+    }
+  }
+`
 
 export default function Header() {
   const { asPath } = useRouter()
@@ -10,14 +43,14 @@ export default function Header() {
   const isCurrent = (route) => asPath.includes(route)
 
   return (
-    <header>
-      <div>
+    <StyledHeader>
+      <div className='title'>
         <Link href='/'>
           <a>Jacob Sullivan</a>
         </Link>
       </div>
 
-      <div>
+      <div className='nav'>
         <nav>
           <Link href='/blog'>
             <a current={isCurrent(routes.blog)}>Blog</a>
@@ -29,6 +62,6 @@ export default function Header() {
 
         <ThemeButton />
       </div>
-    </header>
+    </StyledHeader>
   )
 }
--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro'
 
 const Page = styled.main`
-  ${tw`flex-grow w-4/5`};
+  ${tw`flex-grow w-4/5 py-12`};
 `
 
 export default Page
